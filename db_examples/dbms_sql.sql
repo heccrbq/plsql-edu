d0806e2a9a6ff5de02744f087a8e9ccc20f7ff2c
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-10-27 17:14:52 +0300

    Update dbms_sql.sql

--- db_examples/dbms_sql.sql
+++ db_examples/dbms_sql.sql
@@ -1,3 +1,54 @@
+declare
+    l_cur integer;
+    l_sql varchar2(4000) := 'select dummy, level from dual connect by level <= :1';
+    l_rowcount integer;
+    -- define columns
+    l_colcount integer;
+    l_collist  dbms_sql.desc_tab;
+    l_numval   number;
+    l_var2val  varchar2(50);
+begin
+    l_cur := dbms_sql.open_cursor;
+    dbms_sql.parse(l_cur, l_sql, dbms_sql.native);
+    
+    dbms_sql.describe_columns(l_cur, l_colcount, l_collist);
+--    for i in 1.. l_colcount
+--    loop
+--        if l_collist(i).col_type = 1 then
+--            dbms_sql.define_column(l_cur, i, l_var2val);
+--        elsif l_collist(i).col_type = 2 then
+--            dbms_sql.define_column(l_cur, i, l_numval);
+--        else
+--            dbms_sql.define_column(l_cur, i, l_var2val);
+--        end if;
+--    end loop;
+    
+    dbms_sql.bind_variable(l_cur, ':1', 10);
+    l_rowcount := dbms_sql.execute(l_cur);
+    
+    while dbms_sql.fetch_rows(l_cur) > 0
+    loop
+        for i in 1.. l_colcount
+        loop
+            if l_collist(i).col_type = 1 then
+                dbms_sql.column_value(l_cur, i, l_var2val);
+            elsif l_collist(i).col_type = 2 then
+                dbms_sql.column_value(l_cur, i, l_numval);
+            end if;
+            
+            dbms_output.put_line('column_1: ' || l_var2val || ', columns_2: ' || l_numval);
+        end loop;
+    end loop;
+    
+    dbms_sql.close_cursor(l_cur);
+exception
+    when others then
+        dbms_sql.close_cursor(l_cur);
+end;
+/
+
+/*
+
 PUBLIC.USER_TAB_COLUMNS
     SYS.USER_TAB_COLUMNS
         SYS.USER_TAB_COLS
@@ -35,3 +86,5 @@ PUBLIC.USER_TAB_COLUMNS
                 182 : INTERVAL YEAR TO MONTH
                 183 : INTERVAL DAY TO SECOND
                 208 : UROWID
+
+*/
