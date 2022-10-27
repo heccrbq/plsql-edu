9f07b0d1641b8086732ee9e79c142faceeef8767
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2022-10-27 17:44:59 +0300

    Update dbms_sql.sql

--- db_examples/dbms_sql.sql
+++ db_examples/dbms_sql.sql
@@ -1,3 +1,47 @@
+set timing on
+
+set serveroutput on size unl
+
+
+
+
+-- bulk collect 
+
+declare
+    l_cur integer;
+    l_sql varchar2(4000) := q'[select value(xt).getnumberval() from xmltable('1 to xs:integer(.)' passing :1) xt]';
+    l_rowcount integer;
+    -- define columns
+    l_num_list   dbms_sql.number_table;
+begin
+    l_cur := dbms_sql.open_cursor;
+    dbms_sql.parse(l_cur, l_sql, dbms_sql.native);
+    
+    dbms_sql.bind_variable(l_cur, ':1', 10);
+    
+    dbms_sql.define_array(l_cur, 1, l_num_list, 0, 0); 
+    
+    l_rowcount := dbms_sql.execute(l_cur);
+    
+    while dbms_sql.fetch_rows(l_cur) > 0
+    loop
+        dbms_sql.column_value(l_cur, 1, l_num_list);
+    end loop;
+    
+    dbms_output.put_line(l_num_list.count);
+    
+    dbms_sql.close_cursor(l_cur);
+exception
+    when others then
+        dbms_sql.close_cursor(l_cur);
+end;
+/
+
+
+
+
+
+
 declare
     l_cur integer;
     l_sql varchar2(4000) := 'select dummy, level from dual connect by level <= :1';
@@ -12,16 +56,14 @@ begin
     dbms_sql.parse(l_cur, l_sql, dbms_sql.native);
     
     dbms_sql.describe_columns(l_cur, l_colcount, l_collist);
---    for i in 1.. l_colcount
---    loop
---        if l_collist(i).col_type = 1 then
---            dbms_sql.define_column(l_cur, i, l_var2val);
---        elsif l_collist(i).col_type = 2 then
---            dbms_sql.define_column(l_cur, i, l_numval);
---        else
---            dbms_sql.define_column(l_cur, i, l_var2val);
---        end if;
---    end loop;
+    for i in 1.. l_colcount
+    loop
+        if l_collist(i).col_type = 2 then
+            dbms_sql.define_column(l_cur, i, l_numval);
+        else
+            dbms_sql.define_column(l_cur, i, l_var2val, 50);
+        end if;
+    end loop;
     
     dbms_sql.bind_variable(l_cur, ':1', 10);
     l_rowcount := dbms_sql.execute(l_cur);
@@ -35,9 +77,9 @@ begin
             elsif l_collist(i).col_type = 2 then
                 dbms_sql.column_value(l_cur, i, l_numval);
             end if;
-            
-            dbms_output.put_line('column_1: ' || l_var2val || ', columns_2: ' || l_numval);
         end loop;
+            
+        dbms_output.put_line('column_1: ' || l_var2val || ', columns_2: ' || l_numval);
     end loop;
     
     dbms_sql.close_cursor(l_cur);
@@ -46,6 +88,10 @@ exception
         dbms_sql.close_cursor(l_cur);
 end;
 /
+
+
+
+
 
 /*
 
