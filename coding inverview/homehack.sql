7657c01716dd18a0c971f1a2c069582bb79bf403
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2024-03-03 20:19:41 +0300

    Update homehack.sql

--- coding inverview/homehack.sql
+++ coding inverview/homehack.sql
@@ -34,3 +34,82 @@ end;
 
 select * from table1;
 select * from table2;
+
+
+
+-- 14
+select 
+    string_agg(email, ', ' order by fio)
+from administrators
+
+
+-- 15
+with recursive t as (
+    select date_trunc('year', current_date) dt
+    union all
+    select dt + interval '1 day' dt
+    FROM t
+    WHERE dt < date_trunc('month', current_date) + interval '1 month'
+)
+insert into date_table
+select dt
+from t;
+
+select * from date_table;
+
+
+-- 16
+select
+    (select min(id) from minmax),
+    (select max(id) from minmax)
+
+
+-- 17
+create or replace function factorial(p int) 
+returns int
+language plpgsql
+as
+$$
+begin
+    if p < 0 then
+        raise exception 'Факториал можно вычислить только у натуральных чисел и нуля.';
+    end if;
+     
+    if p = 0 then
+        return 1;
+    else
+        return p * factorial(p-1);
+    end if;
+end;
+$$;
+
+SELECT factorial((SELECT number FROM temp_vars)) AS result;
+
+
+
+-- 18
+create or replace function process_message(p_msg text, p_bit int)
+returns text
+language plpgsql
+as
+$$
+declare
+    result text := p_msg;
+begin
+    if (p_bit & 1) = 1 then
+        result := to_leet_speak(result);
+    end if;
+    
+    if (p_bit & 2) = 2 then
+        result := adjust_text_length(result);
+    end if;
+    
+    if (p_bit & 4) = 4 then
+        result := remove_spaces(result);
+    end if;
+    
+    return result;
+end;
+$$;
+
+SELECT process_message(msg, bit) FROM temp_vars;
