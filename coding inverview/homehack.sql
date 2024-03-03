5e59458205a13bd227fb8252d0eb6e6c6b831b8a
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2024-03-03 20:20:00 +0300

    Update homehack.sql

--- coding inverview/homehack.sql
+++ coding inverview/homehack.sql
@@ -1,18 +1,3 @@
--- Factorial
-create or replace function f (p number) return number
-is
-begin
-    if p < 0 then
-        raise_application_error(-20162, 'Факториал можно вычислить только у натуральных чисел и нуля.', true);
-    elsif p = 0 then
-        return 1;
-    else
-        return p * f(p-1);
-    end if;
-end f;
-/
-
-
 --12
 create table table1 as select * from dual;
 create table table2 as select * from dual;
