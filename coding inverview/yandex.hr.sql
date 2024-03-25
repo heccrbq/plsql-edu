56026e5af7c87ba1770e0f9d8471f323b5117151
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2024-03-25 16:51:29 +0300

    Create yandex.hr.sql

---
+++ coding inverview/yandex.hr.sql
@@ -0,0 +1,39 @@
+-- Сгенерировать последовательность чисел от 1 до 100
+
+select level id from dual connect by level <= 100
+/
+
+with t(id) as (
+    select 1 from dual
+    union all
+    select id+1 from t where id < 100)
+select * from t
+/
+
+select id from dual
+model
+dimension by ( rownum rn)
+measures (1 id)
+rules iterate (101) (
+    id[iteration_number] = iteration_number
+)
+/
+
+select * from xmltable('1 to 100' columns id number path '.')
+/
+
+with function f(p_start number, p_stop number) return sys.odcinumberlist
+is
+    l_result$ sys.odcinumberlist := sys.odcinumberlist();
+begin
+    for i in p_start .. p_stop
+    loop
+        l_result$.extend;
+        l_result$(i) := i;
+    end loop;
+    
+    return l_result$;
+end;
+select * from table(f(1,100));
+/
+
