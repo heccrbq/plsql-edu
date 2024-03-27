1c8eb7163d2287db517d821506059a15b328bc59
Dmitry Bykov <42538722+heccrbq@users.noreply.github.com>
2024-03-27 12:32:32 +0300

    Update yandex.hr-algo.sql

--- coding inverview/yandex.hr-algo.sql
+++ coding inverview/yandex.hr-algo.sql
@@ -1,3 +1,14 @@
+На входе есть строка с числами, разделенными пробелом (одним).
+Нужно написать SQL запрос, который вернет все числа, которые встречаются в строке только один раз.
+Т.е. для строки для строки
+9 83 49 22 2 83 22 833
+запрос должен вернуть (порядок чисел не важен)
+9
+49
+2
+833
+Предпочтительным считается вариант без использования регулярных выражений.
+
 with t as (
     select ' ' || trim(column_value) || ' ' string, 
         length(translate(column_value, '#0123456789','#')) + 1 space_count 
@@ -6,7 +17,7 @@ with t as (
 
 select result
 from (
-    select substr(string, instr(string, ' ', level)+1, instr(string, ' ', level+1) - instr(string, ' ', level) - 1) result 
+    select substr(string, instr(string, ' ', 1, level)+1, instr(string, ' ', 1, level+1) - instr(string, ' ', 1, level) - 1) result 
     from t connect by level <= space_count
 )
 group by result
